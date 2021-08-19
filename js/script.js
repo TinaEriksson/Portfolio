3ec77dc4c4fa98b345419cb0f87225477ad35593
Tina Eriksson <65234428+TinaEriksson@users.noreply.github.com>
2021-08-19 22:53:32 +0200

    added about page, and started fixing it.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggle() {
+  var navMenu = document.getElementById("menu");
+  if (navMenu.style.display === "none") {
+    navMenu.style.display = "flex";
+  } else {
+    navMenu.style.display = "none";
+  }
+}
--- script.js
+++
@@ -1,8 +0,0 @@
-function ToggleMenu() {
-    var x = document.getElementById("social__media");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
-      x.style.display = "none";
-    }
-  }
