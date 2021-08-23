cf336a0d89388ab8d9dd07c4f4eb0c6cd775d55c
Tina Eriksson <65234428+TinaEriksson@users.noreply.github.com>
2021-08-23 16:28:05 +0200

    Removed some pages, and changed the index page

--- js/script.js
+++ js/script.js
@@ -3,30 +3,18 @@ $("#menuButton").click(function(){
 })
 
 
+let btn = document.getElementById("contactBtn");
 
+let xBtn = document.getElementsByClassName("closeBtn")[0];
 
-
-// ***** TAKEN FROM https://www.w3schools.com/howto/howto_css_modals.asp *****// 
-// Get the modal
-var popup = document.getElementById("myPopup");
-
-// Get the button that opens the modal
-var btn = document.getElementById("contactbtn");
-
-// Get the <span> element that closes the modal
-var xBtn = document.getElementsByClassName("closeBtn")[0];
-
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
   popup.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 xBtn.onclick = function() {
   popup.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == popup) {
     popup.style.display = "none";
