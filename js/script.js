79c3614af1f1fb2cb0be6dc2e7101a879279fcca
Tina Eriksson <65234428+TinaEriksson@users.noreply.github.com>
2021-08-26 10:11:16 +0200

    Changes in the structure.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
+/*The code to make the hamburgermenu toggle when you press it. */ 
+
 $("#menuButton").click(function(){
   $("#menu").slideToggle('slow');
 })
-
-
-let btn = document.getElementById("contactBtn");
-
-let xBtn = document.getElementsByClassName("closeBtn")[0];
-
-btn.onclick = function() {
-  popup.style.display = "block";
-}
-
-xBtn.onclick = function() {
-  popup.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == popup) {
-    popup.style.display = "none";
-  }
-}
