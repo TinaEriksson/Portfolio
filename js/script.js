450c5df2af2b59d8527272321335103bdc14a5f9
Tina Eriksson <65234428+TinaEriksson@users.noreply.github.com>
2021-08-26 13:52:07 +0200

    Finished with the foundation of page.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /*The code to make the hamburgermenu toggle when you press it. */ 
 
-$("#menuButton").click(function(){
-  $("#menu").slideToggle('slow');
-})
+$("#menuButton").click(function() {
+    $("#menu").slideToggle('slow');
+});
